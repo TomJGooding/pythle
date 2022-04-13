8e1690de2c19bbd4c3e70c2c531a825115253bc7
TomJGooding <tomgooding91@gmail.com>
2022-04-13 21:12:30 +0100

    Updated init

--- src/project_name/__init__.py
+++ src/project_name/__init__.py
@@ -1,5 +1,6 @@
 """
-Brief description of project
+A terminal-based version of the popular word-guessing game Wordle, built with Python.
+Created as my first GitHub project!
 """
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
