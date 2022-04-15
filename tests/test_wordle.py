28813d3ddd5427d1c6ff5c4a8f34d583a9b6fa5e
TomJGooding <tomgooding91@gmail.com>
2022-04-15 19:23:28 +0100

    Adding branch although know it will fail

--- tests/test_wordle.py
+++ tests/test_wordle.py
@@ -1,1 +1,23 @@
-# TODO: Write tests!
+"""
+HELP!
+I'm struggling understanding pytest and working with packages
+I tried installing with: pip install -e .
+but still errors that the module/function not found
+FAILED tests/test_wordle.py::test_player_stats_str
+- NameError: name 'player_stats_str' is not defined
+"""
+
+# import wordle
+
+
+# def test_player_stats_str():
+#     assert (
+#         player_stats_str(0, 0, 0)
+#         == """
+# ====================================
+#             STATISTICS
+#         0 Played, 0% Wins
+#         Current Streak is 0
+# ====================================
+# """
+#     )
