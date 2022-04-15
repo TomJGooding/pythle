0edf7d4c9c46f2b947eba792668ae699e99fa78b
TomJGooding <tomgooding91@gmail.com>
2022-04-15 20:19:12 +0100

    Added Wordle game with some tests!

--- tests/test_wordle.py
+++ tests/test_wordle.py
@@ -1,23 +1,67 @@
+from wordle.wordle import player_stats_str, guess_count_str, guess_score
+
+
+def test_player_stats_str():
+    assert (
+        player_stats_str(0, 0, 0)
+        == """
+====================================
+            STATISTICS
+        0 Played, 0% Wins
+        Current Streak is 0
+====================================
 """
-HELP!
-I'm struggling understanding pytest and working with packages
-I tried installing with: pip install -e .
-but still errors that the module/function not found
-FAILED tests/test_wordle.py::test_player_stats_str
-- NameError: name 'player_stats_str' is not defined
+    )
+    assert (
+        player_stats_str(2, 1, 0)
+        == """
+====================================
+            STATISTICS
+        2 Played, 50% Wins
+        Current Streak is 0
+====================================
 """
+    )
+    assert (
+        player_stats_str(5, 5, 5)
+        == """
+====================================
+            STATISTICS
+        5 Played, 100% Wins
+        Current Streak is 5
+====================================
+"""
+    )
 
-# import wordle
+
+def test_guess_count_str():
+    assert guess_count_str([]) == "(1 / 6)"
+    assert guess_count_str(["WORDS"]) == "(2 / 6)"
+    assert guess_count_str(["WORDS", "WORDS", "WORDS", "WORDS", "WORDS"]) == "(6 / 6)"
 
 
-# def test_player_stats_str():
-#     assert (
-#         player_stats_str(0, 0, 0)
-#         == """
-# ====================================
-#             STATISTICS
-#         0 Played, 0% Wins
-#         Current Streak is 0
-# ====================================
-# """
-#     )
+def test_guess_score():
+    assert guess_score("AAAAA", "AAAAA") == [
+        {"A": "green"},
+        {"A": "green"},
+        {"A": "green"},
+        {"A": "green"},
+        {"A": "green"},
+    ]
+    assert guess_score("AAAAA", "ZZZZZ") == [
+        {"A": "blue"},
+        {"A": "blue"},
+        {"A": "blue"},
+        {"A": "blue"},
+        {"A": "blue"},
+    ]
+    assert guess_score("ABCDE", "EDCBA") == [
+        {"A": "yellow"},
+        {"B": "yellow"},
+        {"C": "green"},
+        {"D": "yellow"},
+        {"E": "yellow"},
+    ]
+
+
+print(guess_score("ABCDE", "EDCBA"))
